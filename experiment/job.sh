3ae13f548cd4e7d8fe4fa56cc5e0a1d194822ac0
Stefan Kolek <kolek@mathematik.uni-muenchen.de>
2021-11-29 12:05:14 +0100

    edited /gitignore

--- experiment/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-net=$1
-expl=$2
-
-cd /home/math/kolek/dev/CartoonX/experiment
-
-python  make_misclassified_dataset.py --dataset=imagenet --num_imgs=100 --model=$net --expl=$expl --expl_params="hparams/$expl.yaml" 
