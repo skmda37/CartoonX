19c80083ab8dccc33ae90024546ed801cdcd1f67
skmda37 <skmda37@gmail.com>
2026-02-20 17:03:53 +0100

    updated version

--- src/cartoonx/__init__.py
+++ src/cartoonx/__init__.py
@@ -2,4 +2,5 @@ from cartoonx.pipeline.factory import CartoonXFactory
 from cartoonx.modelling.explainer import WaveletBasedCartoonX
 from cartoonx.modelling.explainer import ShearletBasedCartoonX
 
-__version__ = "0.1.0"
+from importlib.metadata import version
+__version__ = version("cartoonx")
